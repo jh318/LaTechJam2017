bea8fd1140e680a50f513cc9d1ee1e8f7f971921
jh318 <jhawley318@gmail.com>
2017-03-31 20:40:58 -0500

    player looks in joystick direction

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
 	public string xAxis = "Horizontal";
 	public string yAxis = "Vertical";
 
+
 	private SpriteRenderer playerSprite; 
 	private Rigidbody2D body;
 
@@ -24,8 +25,12 @@ public class PlayerController : MonoBehaviour {
 	void Update(){
 		float moveHorizontal = Input.GetAxis(xAxis);
 		float moveVert = Input.GetAxis(yAxis);
-
 		body.velocity = new Vector3 (moveHorizontal, moveVert, 0) * speed;
 
+		float aimHorizontal = Input.GetAxis ("RightStickX");
+		float aimVertical = Input.GetAxis ("RightStickY");
+		transform.right = new Vector2 (aimHorizontal, aimVertical);
+		Debug.Log ("x" + aimHorizontal);
+		Debug.Log ("y" + aimVertical);
 	}
 }
