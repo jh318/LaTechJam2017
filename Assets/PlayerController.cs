7a88eb8e56b898b2a05e5e0bf3e8330c306892eb
jh318 <jhawley318@gmail.com>
2017-03-31 20:08:19 -0500

    added player and player basic movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float speed;
+	public Transform visibleBody;
+	public string xAxis = "Horizontal";
+	public string yAxis = "Vertical";
+
+	private SpriteRenderer playerSprite; 
+	private Rigidbody2D body;
+
+
+	private void Start()
+	{
+		playerSprite = GetComponentInChildren<SpriteRenderer>();
+		body = GetComponent<Rigidbody2D>();
+	}
+
+
+
+	void Update(){
+		float moveHorizontal = Input.GetAxis(xAxis);
+		float moveVert = Input.GetAxis(yAxis);
+
+		body.velocity = new Vector3 (moveHorizontal, moveVert, 0) * speed;
+
+	}
+}
