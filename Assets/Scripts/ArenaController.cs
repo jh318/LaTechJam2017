99518a1b8219435b33f7e66129b1bb21146661a2
jh318 <jhawley318@gmail.com>
2017-04-02 00:53:35 -0500

    followers die when they leave the arena

---
+++ Assets/Scripts/ArenaController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaController : MonoBehaviour {
+
+
+	void OnTriggerExit2D(Collider2D c){
+		Debug.Log ("Object left");
+		if (c.gameObject.GetComponent<PlayerFollower> ()) {
+			c.gameObject.SetActive (false);
+			AudioManager.PlayVariedEffect ("WilhelmScream");
+		}
+	}
+}
